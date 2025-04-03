bcd03141f71bc2c9e704e4bd6615fd1272785647
meriem memmiche <meriam_memmiche@ext.carrefour.com>
2025-04-03 17:48:07 +0200

    sepolia

--- frontend/app/utils/client.ts
+++ frontend/app/utils/client.ts
@@ -1,7 +1,15 @@
 import { createPublicClient, http } from "viem"
 import { hardhat } from "wagmi/chains"
+import { sepolia } from "viem/chains"
+
+// export const publicClient = createPublicClient({
+//   chain: hardhat,
+//   transport: http("http://127.0.0.1:8545"),
+// })
 
 export const publicClient = createPublicClient({
-  chain: hardhat,
-  transport: http("http://127.0.0.1:8545"),
+  chain: sepolia,
+  transport: http(
+    "https://eth-sepolia.g.alchemy.com/v2/LjyUqVTQ5d8pBgPvLEUujMxnxoSJIj3M"
+  ),
 })
