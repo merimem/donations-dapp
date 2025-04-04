3940de8e83164715ecb2e2253919ce8ba8128317
meriem memmiche <meriemmemmiche@gmail.com>
2025-04-04 11:20:20 +0200

    change imae and add sepolia network

--- frontend/app/utils/client.ts
+++ frontend/app/utils/client.ts
@@ -2,14 +2,14 @@ import { createPublicClient, http } from "viem"
 import { hardhat } from "wagmi/chains"
 import { sepolia } from "viem/chains"
 
-export const publicClient = createPublicClient({
-  chain: hardhat,
-  transport: http("http://127.0.0.1:8545"),
-})
-
 // export const publicClient = createPublicClient({
-//   chain: sepolia,
-//   transport: http(
-//     "https://eth-sepolia.g.alchemy.com/v2/LjyUqVTQ5d8pBgPvLEUujMxnxoSJIj3M"
-//   ),
+//   chain: hardhat,
+//   transport: http("http://127.0.0.1:8545"),
 // })
+
+export const publicClient = createPublicClient({
+  chain: sepolia,
+  transport: http(
+    "https://eth-sepolia.g.alchemy.com/v2/LjyUqVTQ5d8pBgPvLEUujMxnxoSJIj3M"
+  ),
+})
