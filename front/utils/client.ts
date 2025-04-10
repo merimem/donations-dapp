cd890cdb792e1c3ce30e6805d7eab272ada7881e
meriem memmiche <meriemmemmiche@gmail.com>
2025-04-10 06:40:07 +0200

    Fix pipeline de versionning

--- front/utils/client.ts
+++ front/utils/client.ts
@@ -2,12 +2,12 @@ import { createPublicClient, http } from "viem"
 import { hardhat } from "wagmi/chains"
 import { sepolia } from "viem/chains"
 
-// export const publicClient = createPublicClient({
-//   chain: hardhat,
-//   transport: http("http://127.0.0.1:8545"),
-// })
-
 export const publicClient = createPublicClient({
-  chain: sepolia,
-  transport: http(process.env.RPC_URL),
+  chain: hardhat,
+  transport: http("http://127.0.0.1:8545"),
 })
+
+// export const publicClient = createPublicClient({
+//   chain: sepolia,
+//   transport: http(process.env.RPC_URL),
+// })
--- front/utils/wagmi.ts
+++ front/utils/wagmi.ts
@@ -2,19 +2,19 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit"
 import { http } from "wagmi"
 import { hardhat, sepolia } from "wagmi/chains"
 
-// export const config = getDefaultConfig({
-//   appName: "Good4Chain",
-//   projectId: "YOUR_PROJECT_ID",
-//   chains: [hardhat],
-//   ssr: true,
-// })
-
 export const config = getDefaultConfig({
-  appName: "Chain4Good",
+  appName: "Good4Chain",
   projectId: "YOUR_PROJECT_ID",
-  chains: [sepolia],
-  transports: {
-    [sepolia.id]: http(process.env.RPC_URL),
-  },
+  chains: [hardhat],
   ssr: true,
 })
+
+// export const config = getDefaultConfig({
+//   appName: "Chain4Good",
+//   projectId: "YOUR_PROJECT_ID",
+//   chains: [sepolia],
+//   transports: {
+//     [sepolia.id]: http(process.env.RPC_URL),
+//   },
+//   ssr: true,
+// })
